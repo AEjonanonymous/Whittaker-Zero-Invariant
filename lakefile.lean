39e2a47d10b02eb579c3ec2658a4aa1c8d06e227
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-09-07 22:34:31 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,10 +1,10 @@
 import Lake
 open Lake DSL
 
-package «Whittaker-Zero-Invariant» where
+package «WhittakerZeroInvariant» where
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" 
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
 
 @[default_target]
 lean_lib «ETWhittakerZeroInvariant» where
