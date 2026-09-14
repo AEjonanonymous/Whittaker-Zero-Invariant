59609797310a3313e059a977b6aa22d6d439f0c9
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-09-13 20:49:29 -0400

    Create lakefile.lean

---
+++ Comparator/lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package «WhittakerZeroInvariant» where
+
+require mathlib from git 
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
+
+@[default_target]
+lean_lib «Challenge» where
+  srcDir := "."
+
+lean_lib «Solution» where
+  srcDir := "."
